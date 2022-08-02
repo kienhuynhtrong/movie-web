44a028bbb67d40bed54a3f63b3705f0bcb71062a
kienhuynh <kienhuynhtrong@gmail.com>
2022-08-02 22:00:35 +0700

    commit code add taiwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  content: [
-    // add this lines
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    // if you have other folder, add its path too, like this:
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      colors: {
+        "dark-lighten": "#2a2a2a",
+        dark: "#222222",
+        "dark-darken": "#1a1a1a",
+        orange: "#ff4112",
+        red: "#FF0000",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
